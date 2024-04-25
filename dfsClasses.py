1b1fc67356c6737859879b2beba2512a11ca9248
Matt <matthewquinones42@gmail.com>
2024-04-24 20:13:52 -0600

    documentation

---
+++ dfsClasses.py
@@ -0,0 +1,22 @@
+import time
+
+# doubly ended queue: allows element appending and popping on both sides of the queue
+from collections import deque
+
+
+# checks if the position passed in is a valid move or not
+def isValidMove(position):
+    (x,y) = position
+    xValid = (x<=40) & (x>=0) # if x is is within grid dimensions and that it does not go backwards
+    yValid = (y<=40) & (y>=0) # if y is is within grid dimensions and that it does not go backwards
+    if (xValid and yValid): 
+        return True
+    else: 
+        return False
+    
+class DFS:
+    def __init__(self, startPosition, goalPosition):
+        self.frontier = deque()
+        self.visited = set()
+        self.startPosition = startPosition
+        self.goalPosition = goalPosition
